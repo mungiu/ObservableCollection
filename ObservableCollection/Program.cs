67d035a3ae36468a805beb6910943c3b9195ef91
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-03 00:01:59 +0200

    Tutorial checkpoint

--- ObservableCollection/Program.cs
+++ ObservableCollection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace ObservableCollection
 {
@@ -6,7 +8,28 @@ namespace ObservableCollection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ObservableCollection<string> presidents = new
+                ObservableCollection<string>
+            {
+                "Jimmy Carter",
+                "Ronald Reagan",
+                "George HW Bush"
+            };
+
+            //creating current action notification
+            presidents.CollectionChanged += OnCollectionChanged;
+
+            presidents.Add("Bill Clinton");
+            presidents.Remove("Jimmy Carter");
+
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+        }
+
+        //providing data for the notification
+        static void OnCollectionChanged(object sender,
+            NotifyCollectionChangedEventArgs e)
+        {
         }
     }
 }
