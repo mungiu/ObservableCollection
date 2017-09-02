315d1e301ebce7434a04120f3106a1016a1cfe50
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-02 23:39:52 +0200

    Add project files.

---
+++ ObservableCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObservableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
